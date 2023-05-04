e5c43326c166dd3c7414f384e4d7c6c6cd5130a2
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-05-04 23:49:43 +0530

    Add: Clear all, Show input, Show result, Backspace functionality

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const digits = [];
-const numbers = {};
-const checkOperators = /[+\%-*/x]/;
 
+const equation = {
+    firstNumber: 0,
+    operator: null,
+    secondNumber: 0,
+};
+let operatorCounter = 0;
+let operator;
+
+const checkOperators = ['-', 'x', '/', '+', '%'];
 const day = document.querySelector('#day');
 const night = document.querySelector('#night');
 const mode = document.querySelector('.modes');
@@ -10,8 +16,8 @@ const credit = document.querySelector('.credit');
 const buttons = document.querySelectorAll('.btn');
 const buttonsBg = document.querySelector('.btn-container');
 const containerBg = document.querySelector('.container');
-const displayEq = document.querySelector('.big'); // Current equation entered
-const displayEv = document.querySelector('.small'); // Evaluated result
+const displayBig = document.querySelector('.big'); // Current equation entered
+const displaySmall = document.querySelector('.small'); // Evaluated result
 
 
 function add(num1, num2) {
@@ -43,43 +49,88 @@ function changeColor() {
     credit.classList.toggle('day');
     containerBg.classList.toggle('day');
     buttonsBg.classList.toggle('day');
-    displayEq.classList.toggle('day');
-    displayEv.classList.toggle('day');
+    displayBig.classList.toggle('day');
+    displaySmall.classList.toggle('day');
 }
 
 function showInput(e) {
-    if (e.target.id === 'ac') clearAll();
-    else if (e.target.id === 'c') backspace();
-    else displayEq.textContent += `${e.target.id}`;
-    
+    if (checkOperators.includes(e.target.id) || e.target.id) {
+        displayBig.textContent += `${e.target.id}`;
+    }
+}
+
+function showResult() {
+        displaySmall.textContent = `${equation.firstNumber} ${equation.operator} ${equation.secondNumber}`;
 }
 
 function clearAll() {
-    displayEq.textContent = '';
-    displayEv.textContent = '';
+    displayBig.textContent = '';
+    displaySmall.textContent = '';
+    equation.firstNumber = 0;
+    equation.secondNumber = 0;
+    equation.operator = null;
 }
 
 function backspace() {
-    const equation = displayEq.textContent;
-    displayEq.textContent = equation.slice(0, equation.length - 1);
+    const equation = displayBig.textContent;
+    displayBig.textContent = equation.slice(0, equation.length - 1);
 }
 
-function operate(e) {
-    
-    // Since the value of 0 is false so we add 1 to it pass through the condition
-    if (Number(e.target.id) + 1 || e.target.id === '.') {
+const digits = [];
+function getInput(e) {
+    // Since the value of 0 is false so we add 1 to make the condition true
+    if ((Number(e.target.id) + 1 || e.target.id === '.') && !equation.operator) {
         digits.push(e.target.id);
-    } else if (checkOperators.test(e.target.id)) {
-        console.log(e.target.id);
+        equation.firstNumber = parseFloat(digits.join(''));
+        showInput(e);
     }
-    numbers['firstNumber'] = Number(digits.join(''));
-    // const firstNumber =
-    // const secondNumber = 
-    // const operator = ;
-    console.log(numbers.firstNumber);
-    showInput(e);
+    else if (checkOperators.includes(e.target.id)) {
+        equation.operator = e.target.id;
+        digits.length = 0;
+        showInput(e);
+
+    }
+    else if ((Number(e.target.id) + 1 || e.target.id === '.') && equation.operator) {
+        digits.push(e.target.id);
+        equation.secondNumber = parseFloat(digits.join(''));
+        showInput(e);
+    }
+    else if (e.target.id === 'ac') {
+        clearAll();
+    }
+    else if (e.target.id === 'c') {
+        if (!displayBig.textContent && displaySmall) {
+            const value = displaySmall.textContent;
+            displaySmall.textContent = '';
+            displayBig.textContent = value.split(' ').join('');
+        } else backspace();
+    }
+    console.log(equation.firstNumber, equation.operator, equation.secondNumber);
+    if (e.target.id === '=') {
+        displayBig.textContent = operate();
+        digits.length = 0;
+        showResult();
+    }
+}
+
+function operate() {
+    switch (equation.operator) {
+        case '+':
+            return add(equation.firstNumber, equation.secondNumber);
+        case '-':
+            displayBig.textContent = subtract(equation.firstNumber, equation.secondNumber);
+            break;
+        case 'x':
+            displayBig.textContent = multiply(equation.firstNumber, equation.secondNumber);
+            break;
+        case '/':
+            displayBig.textContent = divide(equation.firstNumber, equation.secondNumber);
+            break;
+        case '%':
+            displayBig.textContent = percentage(equation.firstNumber, equation.secondNumber);
+    }
 }
 
 day.addEventListener('click', changeColor);
 night.addEventListener('click', changeColor);
-buttons.forEach((btn) => btn.addEventListener('click', operate));
+buttons.forEach((btn) => btn.addEventListener('click', getInput));
